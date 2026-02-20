1681587c1d825895c8b857a6605100ea5cd4bc8b
Deepanshu Mishra <deep270804@gmail.com>
2026-02-21 02:15:32 +0530

    ++

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser) {
-sh "sudo docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String project, String imageTag, String DockerHubUser) {
+sh "sudo docker build -t ${DockerHubUser}/${project}:${imageTag} ."
 }
--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
-def call(String project, String ImageTag, String dockerHubUser) {
+def call(String project, String imageTag, String dockerHubUser) {
 withCredentials ([usernamePassword (credentialsId: 'DockerHubCred', passwordVariable: 'DockerHubCredPass', usernameVariable: 'DockerHubCredUser')]){
 sh "sudo docker login -u ${DockerHubCredUser} -p ${DockerHubCredPass}"
-sh "sudo docker push ${DockerHubCredUser}/${Project}:${ImageTag}"
+sh "sudo docker push ${DockerHubCredUser}/${project}:${imageTag}"
 }
 }
